df853d7e5fc9f5e12791a31282f6ae745c4f1867
lefebvredavid59 <dlefebvredev@gmail.com>
2020-02-17 12:07:44 +0100

    --

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,22 +2,53 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
 use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class IndexController extends AbstractController
 {
     /**
      * @Route("/", name="index")
      */
-    public function index(ProgramRepository $programRepository)
+    public function index(ProgramRepository $programRepository,
+                          Request $request,
+                          UserPasswordEncoderInterface $passwordEncoder)
     {
       $programs = $programRepository->findAll();
+
+      // Form Inscription
+
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('index');
+        }
 
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
             'programs' => $programs,
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
