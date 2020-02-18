cd0de082fc6005a410d765fa35f4a4e3b543495c
Nicolas Rousseaux <rousseaux.nicolas@outlook.com>
2020-02-18 13:46:28 +0100

    mise en forme formulaire programe

--- src/Migrations/Version20200218091214.php
+++ src/Migrations/Version20200218091214.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200217125750 extends AbstractMigration
+final class Version20200218091214 extends AbstractMigration
 {
     public function getDescription() : string
     {
