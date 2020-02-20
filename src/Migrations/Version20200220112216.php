8a5938aadb1a743b85ff7896621802f9a56c4a27
Dewalle Julien <julien.dewalle@hotmail.fr>
2020-02-20 12:26:49 +0100

    modify front

--- src/Migrations/Version20200220112216.php
+++ src/Migrations/Version20200220112216.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200220111215 extends AbstractMigration
+final class Version20200220112216 extends AbstractMigration
 {
     public function getDescription() : string
     {
