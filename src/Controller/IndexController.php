a08add70231363e1aba8ca0cb9fa1ac597070c52
lefebvredavid59 <dlefebvredev@gmail.com>
2020-02-20 11:33:43 +0100

    add contact form

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\ContactType;
 use App\Form\RegistrationFormType;
 use App\Repository\ProgramRepository;
 use App\Repository\WorkshopRepository;
@@ -10,8 +11,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 
 class IndexController extends AbstractController
 {
@@ -21,12 +24,15 @@ class IndexController extends AbstractController
      * @param Request $request
      * @param UserPasswordEncoderInterface $passwordEncoder
      * @param WorkshopRepository $workshopRepository
+     * @param MailerInterface $mailer
      * @return RedirectResponse|Response
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
      */
     public function index(ProgramRepository $programRepository,
                           Request $request,
                           UserPasswordEncoderInterface $passwordEncoder,
-                          WorkshopRepository $workshopRepository)
+                          WorkshopRepository $workshopRepository,
+                          MailerInterface $mailer)
     {
       $programs = $programRepository->findAll();
       $workshops = $workshopRepository->findAll();
@@ -55,11 +61,30 @@ class IndexController extends AbstractController
             return $this->redirectToRoute('index');
         }
 
+        //Contact Form
+
+        $contact = $this->createForm(ContactType::class);
+        $contact->handleRequest($request);
+
+        if ($contact->isSubmitted() && $contact->isValid()) {
+            $email = (new TemplatedEmail())
+                ->from('hello@example.com')
+                ->to($user->getMail())
+                ->subject('Mail confirmation')
+                ->htmlTemplate('mail/confirmMail.html.twig')
+                ->context([
+                    'user' => $user,
+                ]);
+            $mailer->send($email);
+        }
+
+
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
             'programs' => $programs,
             'workshops' => $workshops,
             'registrationForm' => $form->createView(),
+            'contactform' => $contact->createView(),
         ]);
     }
     /**
